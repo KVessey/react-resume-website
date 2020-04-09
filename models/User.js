19fbabfad4a0044ac9d0820f9fe8f3434abafef6
Kyle <k.vessey@gmail.com>
2020-04-09 15:59:18 -0500

    Add validation to register a user

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// set User variable to mongoose model name user and schema we just created
+module.exports = User = mongoose.model('user', UserSchema);

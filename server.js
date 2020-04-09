6b1f6aba7846f13ebe3c1a04f1c114fa21ec9975
Kyle <k.vessey@gmail.com>
2020-04-09 15:33:03 -0500

    Route files with express router to init apis

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+// This uses the api/users js file in the routes directory so we can use this api functionality in our app
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 // This gets the environment port when deployed to heroku, or it will run on port 5000 by default
 const PORT = process.env.PORT || 5000;
 

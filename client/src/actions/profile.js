b0e61e8341f9cc41c60e2e73307b0b5a0cdf9395
Kyle <k.vessey@gmail.com>
2020-04-20 00:49:56 -0500

    displaying github repos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -64,7 +64,7 @@ export const getProfileById = (userId) => async (dispatch) => {
 };
 
 // Get GitHub repos
-export const getGirhubRepos = (username) => async (dispatch) => {
+export const getGithubRepos = (username) => async (dispatch) => {
   try {
     const res = await axios.get(`/api/profile/github/${username}`);
 

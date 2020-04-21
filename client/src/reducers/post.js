a940d35abdfc33897bd1c75e59cb15472b431275
Kyle <k.vessey@gmail.com>
2020-04-20 20:37:14 -0500

    adding comments

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -5,6 +5,8 @@ import {
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from '../actions/types';
 
 const initialState = {
@@ -58,6 +60,23 @@ export default function (state = initialState, action) {
           post._id === payload.id ? { ...post, likes: payload.likes } : post
         ),
         loading: false,
+      };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false,
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            (comment) => comment._id !== payload // Bring in all comments that dont equal the payload sent in the dispatcher in action file
+          ),
+          loading: false,
+        },
       };
     default:
       return state;
